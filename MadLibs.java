65fee79009629339122f82ce6468c18d4ff8b246
Elco Garcia <garciaelco18@gmail.com>
2019-10-24 17:03:37 -0500

    fun madlib game

---
+++ MadLibs.java
@@ -0,0 +1,31 @@
+public class MadLibs {
+    /*
+     * This program generates a mad libbed story. Author: Elco
+     */
+    public static void main(String[] args) {
+        String name1 = "Arthur";
+        String adjective1 = "hungry";
+        String adjective2 = "food";
+        String adjective3 = "big";
+        String verb1 = "drop";
+        String noun1 = "library";
+        String noun2 = "Disneyland";
+        String noun3 = "love child";
+        String noun4 = "balloon";
+        String noun5 = "band";
+        String noun6 = "knife";
+        String name2 = "Billy Bob";
+        int number = 69;
+        String place1 = "Yo momma";
+
+        // The template for the story
+        String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a "
+                + adjective2 + " day!' Outside, a bunch of " + noun1 + "s were protesting to keep " + noun2
+                + " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the "
+                + noun4 + "s very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1
+                + " to " + place1 + " and dropped " + name1 + " in a puddle of frozen " + noun5 + ". " + name1
+                + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+
+        System.out.println(story);
+    }
+}
